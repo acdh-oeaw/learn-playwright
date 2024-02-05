34478a4c91229053a7a3a19380df5c53cb35e9ef
Stefan Probst <stefanprobst@posteo.de>
2024-02-05 15:11:17 +0100

    test: add e2e tests

--- e2e/app.test.ts
+++ e2e/app.test.ts
@@ -1,35 +1,72 @@
-/* eslint-disable playwright/expect-expect */
+import { expect, test } from "@playwright/test";
 
-import { test } from "@playwright/test";
-
-test.fixme("should display initial search results on page load", () => {
-	// TODO:
+test("should display initial search results on page load", async ({ page }) => {
+	await page.goto("/");
+	await page.waitForResponse("/api/cats");
+	await expect(page.getByRole("listitem")).toHaveCount(20);
 });
 
-test.fixme("should allow filtering search results by kind", () => {
-	// TODO:
+test("should allow filtering search results by kind", async ({ page }) => {
+	await page.goto("/");
+	await page.waitForResponse("/api/cats");
+	await page.getByRole("combobox", { name: "Cat kind" }).selectOption("Himalayan");
+	await page.getByRole("button", { name: "Search" }).click();
+	await page.waitForResponse("/api/cats?kind=Himalayan");
+	await expect(page.getByRole("listitem").first()).toContainText("(Himalayan)");
+	await expect(page).toHaveURL('/?kind=Himalayan')
 });
 
-test.fixme("should display a loading spinner while fetching search results", () => {
-	// TODO:
+test("should display a loading spinner while fetching search results", async ({ page }) => {
+	const spinner = page.locator('[role="status"] > svg');
+
+	await page.goto("/");
+	await expect(spinner).toBeVisible();
+	await page.waitForResponse("/api/cats");
+	await expect(spinner).not.toBeVisible();
+
+	await page.getByRole("combobox", { name: "Cat kind" }).selectOption("Himalayan");
+	await page.getByRole("button", { name: "Search" }).click();
+	await expect(spinner).toBeVisible();
+	await page.waitForResponse("/api/cats?kind=Himalayan");
+	await expect(spinner).not.toBeVisible();
 });
 
-test.fixme("should display an error when fetching search results failed", async ({ page }) => {
-	// TODO:
+test("should display an error when fetching search results failed", async ({ page }) => {
+	await page.route("/api/cats", (route) => {
+		return route.fulfill({ status: 500 });
+	});
+
+	await page.goto("/");
+	await page.waitForResponse("/api/cats");
+	await expect(page.getByRole("main")).toContainText("Internal Server Error");
 });
 
-test.fixme("should set lang attribute on html element", () => {
-	// TODO: test if the html element has a lang="en" attribute
+test("should set lang attribute on html element", async ({ page }) => {
+	await page.goto("/");
+	await expect(page.locator("html")).toHaveAttribute("lang", "en");
 });
 
-test.fixme("should have a skiplink", () => {
-	// TODO: test if the first keyboard-focusable element is a link, which jumps to `<main>`
+test("should have a skiplink", async ({ page }) => {
+	const link = page.getByRole("link", { name: "Skip to main content" });
+
+	await page.goto("/");
+	await page.keyboard.press("Tab");
+	await expect(link).toBeFocused();
+
+	await link.click();
+	await expect(page.getByRole("main")).toBeFocused();
 });
 
-test.fixme("should have a favicon", () => {
-	// TODO: test if a request to favicon.ico returns a success http status code
+test("should have a favicon", async ({ request }) => {
+	const response = await request.get("/favicon.ico");
+	await expect(response.status()).toBe(200);
 });
 
-test.fixme("should have schema.org metadata", () => {
-	// TODO: test if page includes a `<script type="application/ld+json">` with metadata
+test("should have schema.org metadata", async ({ page }) => {
+	await page.goto("/");
+	const metadata = await page.locator('script[type="application/ld+json"]').evaluate((element) => {
+		console.log(element)
+		return element.innerHTML;
+	});
+	await expect(metadata).toMatchSnapshot();
 });
